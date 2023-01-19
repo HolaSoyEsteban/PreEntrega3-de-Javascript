015c9921e0c023c8b4d2db4761b438dcf56bb843
Esteban Cardozo <cardozo.esteban@gmail.com>
2023-01-19 09:21:27 -0300

    Menu mejorado rellenandose directamente de los arrays con objetos

--- js/main.js
+++ js/main.js
@@ -31,8 +31,8 @@ function menuSagas() {
     alert("Opción elegida: (1 - Ver Libros disponibles)");
 
     do{
-        console.log("\nElija del 1 al 6 la colección que desea agregar al carrito:\n\nSagas disponibles:\n\n1 - Saga completa de Harry Potter\n2 - Saga completa de Maze Runner\n3 - Saga completa de Sherlock Holmes\n4 - Colección de libros de Stephen King\n5 - Colección de libros de Julio Verne\n6 - Volver al menú principal");
-        let opcionSagas = parseInt(prompt("Elija del 1 al 6 la colección que desea agregar al carrito:\n\nSagas disponibles:\n\n1 - Saga completa de Harry Potter\n2 - Saga completa de Maze Runner\n3 - Saga completa de Sherlock Holmes\n4 - Colección de libros de Stephen King\n5 - Colección de libros de Julio Verne\n6 - Volver al menú principal"));
+        console.log(`\nElija del 1 al 6 la colección que desea agregar al carrito:\n\nSagas disponibles:\n\n1 - ${Libros[0].nombre} Precio: $ ${Libros[0].precio} Stock: ${Libros[0].stock} \n2 - ${Libros[1].nombre} Precio: $ ${Libros[1].precio} Stock: ${Libros[1].stock} \n3 - ${Libros[2].nombre} Precio: $ ${Libros[2].precio} Stock: ${Libros[2].stock} \n4 - ${Libros[3].nombre} Precio: $ ${Libros[3].precio} Stock: ${Libros[3].stock} \n5 - ${Libros[4].nombre} Precio: $ ${Libros[4].precio} Stock: ${Libros[4].stock} \n6 - Volver al menú principal`);
+        let opcionSagas = parseInt(prompt(`Elija del 1 al 6 la colección que desea agregar al carrito:\n\nSagas disponibles:\n\n1 - ${Libros[0].nombre} Precio: $ ${Libros[0].precio} Stock: ${Libros[0].stock} \n2 - ${Libros[1].nombre} Precio: $ ${Libros[1].precio} Stock: ${Libros[1].stock} \n3 - ${Libros[2].nombre} Precio: $ ${Libros[2].precio} Stock: ${Libros[2].stock} \n4 - ${Libros[3].nombre} Precio: $ ${Libros[3].precio} Stock: ${Libros[3].stock} \n5 - ${Libros[4].nombre} Precio: $ ${Libros[4].precio} Stock: ${Libros[4].stock} \n6 - Volver al menú principal`));
         if(opcionSagas >= 1 && opcionSagas <= 6){
             return opcionSagas;
         }else{
@@ -41,6 +41,43 @@ function menuSagas() {
         }
     }while(opcionSagas != 1 || opcionSagas != 2 || opcionSagas != 3 || opcionSagas != 4 || opcionSagas != 5 || opcionSagas != 6);
 }
+//   Funcion para cancelar la compra
+function borrarCarrito(){
+    console.log("\nDesea cancelar la compra? Escriba 'SI' para salir o 'NO' para para volver al menú\n");
+    preguntaCancelarCompra = prompt("\nDesea cancelar la compra? Escriba 'SI' para salir o 'NO' para para volver al menú\n");
+    preguntaCancelarCompra = preguntaCancelarCompra.toUpperCase();
+            
+    console.log("\nLa opción ingresada fue: " + preguntaCancelarCompra);
+
+    if(preguntaCancelarCompra === "SI"){
+        console.log("\nCancelando...\n");
+        alert("Cancelando...");
+                
+        while(Carrito.length > 0){
+            Carrito.pop();
+        };
+
+        console.log("\nCompra Cancelada con exito\n");
+        alert("Compra Cancelada con exito");
+    }else if(preguntaCancelarCompra === "NO"){
+        console.log("\nVolviendo al menú principal\n");
+        alert("\nVolviendo al menú principal\n");
+    }else{
+        console.log("\nLa opción ingresada es incorrecta\n");
+        alert("\nLa opción ingresada es incorrecta\n");
+    }
+}
+//   Funcion ver total a pagar
+function precioTotal(){
+    let totalAPagar = 0;
+    for(const producto of Carrito){
+        totalAPagar = totalAPagar + (producto.precio * producto.Cantidad);
+    }
+
+    console.log("Total de la compra = $ " + totalAPagar);
+    alert(`Total de la compra = $ ${totalAPagar}`);
+}
+
 // ------------------------------------------------------------------------------
 // ARRAYS Y OBJETOS
 
@@ -51,15 +88,18 @@ class Saga {
         this.precio = precio;
         this.stock = stock;
     }
+    descontarDelStock(cantidadADescontar){
+        this.stock = stock - cantidadADescontar;
+    }
 }
 
 //   Arrays
 const Libros = [
-    new Saga("Harry Potter", 10000, 20),
-    new Saga("Maze Runner", 7000, 30),
-    new Saga("Sherlock Holmes", 15000, 10),
-    new Saga("Stephen King", 20000, 10),
-    new Saga("Julio Verne", 13000, 20),
+    new Saga("Saga Harry Potter", 10000, 20),
+    new Saga("Saga Maze Runner", 7000, 30),
+    new Saga("Saga Sherlock Holmes", 15000, 10),
+    new Saga("Saga Stephen King", 20000, 10),
+    new Saga("Saga Julio Verne", 13000, 20),
 ]
 
 const Carrito = [];
@@ -80,7 +120,7 @@ let preguntaRealizarCompra = "NO";
 let preguntaSalir = "NO";
 
 
-saludo();
+// saludo();
 
 do{
     opcionElegida = menuPrincipal();
@@ -127,72 +167,50 @@ do{
             console.log("\nOpción elegida: (2 - Mostrar carrito de compra)\n");
             alert("Opción elegida: (2 - Mostrar carrito de compra)");
 
+            arrayEnTextoParaUnSoloAlert = "";
             Carrito.forEach((producto)=>{
                 console.log(producto);
-                // alert("Saga: " + producto.nombre + " Precio: $" + producto.precio + " Cantidad: " + producto.Cantidad);
-                arrayEnTextoParaUnSoloAlert = `${arrayEnTextoParaUnSoloAlert} \nSaga: ${producto.nombre} Precio: ${producto.precio} Cantidad: ${producto.Cantidad}`;
+                arrayEnTextoParaUnSoloAlert = ` ${arrayEnTextoParaUnSoloAlert} \nSaga: ${producto.nombre} Precio: ${producto.precio} Cantidad: ${producto.Cantidad}`;
             });
             alert(arrayEnTextoParaUnSoloAlert);
+            precioTotal();
             
         break;
         case 3:
-            // console.log("\nOpción elegida: (3 - Cancelar la compra)\n");
-            // alert("Opción elegida: (3 - Cancelar la compra)");
+            console.log("\nOpción elegida: (3 - Cancelar la compra)\n");
+            alert("Opción elegida: (3 - Cancelar la compra)");
 
-            // console.log("\nDesea cancelar la compra? Escriba 'SI' para salir o 'NO' para para volver al menú\n");
-            // preguntaCancelarCompra = prompt("\nDesea cancelar la compra? Escriba 'SI' para salir o 'NO' para para volver al menú\n");
-            // preguntaCancelarCompra = preguntaCancelarCompra.toUpperCase();
-            
-            // console.log("\nLa opción ingresada fue: " + preguntaCancelarCompra);
-
-            // if(preguntaCancelarCompra === "SI"){
-            //     console.log("\nCancelando...\n");
-            //     alert("Cancelando...");
-            //     cantidadHarryPotter = 0;
-            //     cantidadMazeRunner = 0;
-            //     cantidadSherlockHolmes = 0;
-            //     cantidadStephenKing = 0;
-            //     cantidadJulioVerne = 0;
-                
-            //     HarryPotter = false;
-            //     MazeRunner = false;
-            //     SherlockHolmes = false;
-            //     StephenKing = false;
-            //     JulioVerne = false;
-
-            //     console.log("\nCompra Cancelada con exito\n");
-            //     alert("Compra Cancelada con exito");
-            // }else if(preguntaCancelarCompra === "NO"){
-            //     console.log("\nVolviendo al menú principal\n");
-            //     alert("\nVolviendo al menú principal\n");
-            // }else{
-            //     console.log("\nLa opción ingresada es incorrecta\n");
-            //     alert("\nLa opción ingresada es incorrecta\n");
-            // }
+            borrarCarrito();
         break;
         case 4:
-            // console.log("\nOpción elegida: (4 - Realizar la compra)\n");
-            // alert("Opción elegida: (4 - Realizar la compra)");
+            console.log("\nOpción elegida: (4 - Realizar la compra)\n");
+            alert("Opción elegida: (4 - Realizar la compra)");
 
-            // console.log("\nDesea realizar la compra? Escriba 'SI' para seguir o 'NO' para para volver al menú\n");
-            // preguntaRealizarCompra = prompt("\nDesea realizar la compra? Escriba 'SI' para seguir o 'NO' para para volver al menú\n");
-            // preguntaRealizarCompra = preguntaRealizarCompra.toUpperCase();
+            precioTotal();
+            console.log("\nDesea realizar la compra? Escriba 'SI' para seguir o 'NO' para para volver al menú\n");
+            preguntaRealizarCompra = prompt("\nDesea realizar la compra? Escriba 'SI' para seguir o 'NO' para para volver al menú\n");
+            preguntaRealizarCompra = preguntaRealizarCompra.toUpperCase();
             
-            // console.log("\nLa opción ingresada fue: " + preguntaRealizarCompra);
+            console.log("\nLa opción ingresada fue: " + preguntaRealizarCompra);
 
-            // if(preguntaRealizarCompra === "SI"){
-            //     console.log("\nProcesando...\n");
-            //     alert("Procesando...");
+            if(preguntaRealizarCompra === "SI"){
+                console.log("\nProcesando...\n");
+                alert("Procesando...");
+                console.log("\nCompra Realizada con exito\n");
+                alert("Compra Realizada con exito");
+
+                ActualizarStock();
                 
-            //     console.log("\nCompra Realizada con exito\n");
-            //     alert("Compra Realizada con exito");
-            // }else if(preguntaRealizarCompra === "NO"){
-            //     console.log("\nVolviendo al menú principal\n");
-            //     alert("\nVolviendo al menú principal\n");
-            // }else{
-            //     console.log("\nLa opción ingresada es incorrecta\n");
-            //     alert("\nLa opción ingresada es incorrecta\n");
-            // }
+                while(Carrito.length > 0){
+                    Carrito.pop();
+                };
+            }else if(preguntaRealizarCompra === "NO"){
+                console.log("\nVolviendo al menú principal\n");
+                alert("\nVolviendo al menú principal\n");
+            }else{
+                console.log("\nLa opción ingresada es incorrecta\n");
+                alert("\nLa opción ingresada es incorrecta\n");
+            }
             
         break;
         case 5:

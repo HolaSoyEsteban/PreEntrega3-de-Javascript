3349e031a4561151fabbada0096771ca4beee103
Esteban Cardozo <cardozo.esteban@gmail.com>
2023-01-19 05:26:05 -0300

    Mejorando el carrito de compras y el alert

--- js/main.js
+++ js/main.js
@@ -41,7 +41,29 @@ function menuSagas() {
         }
     }while(opcionSagas != 1 || opcionSagas != 2 || opcionSagas != 3 || opcionSagas != 4 || opcionSagas != 5 || opcionSagas != 6);
 }
-/*
+// ------------------------------------------------------------------------------
+// ARRAYS Y OBJETOS
+
+//   Objetos
+class Saga {
+    constructor(nombre, precio, stock){
+        this.nombre = nombre;
+        this.precio = precio;
+        this.stock = stock;
+    }
+}
+
+//   Arrays
+const Libros = [
+    new Saga("Harry Potter", 10000, 20),
+    new Saga("Maze Runner", 7000, 30),
+    new Saga("Sherlock Holmes", 15000, 10),
+    new Saga("Stephen King", 20000, 10),
+    new Saga("Julio Verne", 13000, 20),
+]
+
+const Carrito = [];
+
 // ----------------------------------------------------------------------------
 
 // Programa principal
@@ -49,44 +71,14 @@ function menuSagas() {
  // Variables
 let opcionElegida = 0;
 let opcionSagaElegida = 0;
-
-let suma = 0;
-*/
-
-class Saga {
-    constructor(nombre,precio){
-        this.nombre = nombre;
-        this.precio = precio;
-    }
-}
-
-const Libros = [
-    new Saga("Harry Potter", 10000),
-    new Saga("Maze Runner", 7000),
-    new Saga("Sherlock Holmes", 15000),
-    new Saga("Stephen King", 20000),
-    new Saga("Julio Verne", 13000),
-]
-
-console.log(Libros);
-/*
-
-let cantidadHarryPotter = 0;
-let cantidadMazeRunner = 0;
-let cantidadSherlockHolmes = 0;
-let cantidadStephenKing = 0;
-let cantidadJulioVerne = 0;
-
-let HarryPotter = false;
-let MazeRunner = false;
-let SherlockHolmes = false;
-let StephenKing = false;
-let JulioVerne = false;
+let cantidadQueVaAComprar = 0;
+let arrayEnTextoParaUnSoloAlert = "";
 let salir = false;
 
 let preguntaCancelarCompra = "NO";
 let preguntaRealizarCompra = "NO";
 let preguntaSalir = "NO";
+
 
 saludo();
 
@@ -99,39 +91,34 @@ do{
             if(opcionSagaElegida >= 1 && opcionSagaElegida <= 5){
                 switch(opcionSagaElegida){
                     case 1:
-                        suma = suma + precioHarryPotter;
-                        HarryPotter = true;
-                        cantidadHarryPotter++;
-                        console.log("\nAgregado 1 pack de Harry Potter\n");
-                        alert("Agregado 1 pack de Harry Potter");
+                        cantidadQueVaAComprar = parseInt(prompt("Ingrese la cantidad que desea llevar: "));
+                        Carrito.push({nombre: Libros[0].nombre, precio: Libros[0].precio, Cantidad: cantidadQueVaAComprar});
+                        console.log("\nAgregado al carrito\n");
+                        alert("Agregado al carrito");
                     break;
                     case 2:
-                        suma = suma + precioMazeRunner;
-                        MazeRunner = true;
-                        cantidadMazeRunner++;
-                        console.log("\nAgregado 1 pack de Maze Runner\n");
-                        alert("Agregado 1 pack de Maze Runner");
+                        cantidadQueVaAComprar = parseInt(prompt("Ingrese la cantidad que desea llevar: "));
+                        Carrito.push({nombre: Libros[1].nombre, precio: Libros[1].precio, Cantidad: cantidadQueVaAComprar});
+                        console.log("\nAgregado al carrito\n");
+                        alert("Agregado al carrito");
                     break;
                     case 3:
-                        suma = suma + precioSherlockHolmes;
-                        SherlockHolmes = true;
-                        cantidadSherlockHolmes++;
-                        console.log("\nAgregado 1 pack de Sherlock Holmes\n");
-                        alert("Agregado 1 pack de Sherlock Holmes");
+                        cantidadQueVaAComprar = parseInt(prompt("Ingrese la cantidad que desea llevar: "));
+                        Carrito.push({nombre: Libros[2].nombre, precio: Libros[2].precio, Cantidad: cantidadQueVaAComprar});
+                        console.log("\nAgregado al carrito\n");
+                        alert("Agregado al carrito");
                     break;
                     case 4:
-                        suma = suma + precioStephenKing;
-                        StephenKing = true;
-                        cantidadStephenKing++;
-                        console.log("\nAgregado 1 pack de Stephen King\n");
-                        alert("Agregado 1 pack de Stephen King");
+                        cantidadQueVaAComprar = parseInt(prompt("Ingrese la cantidad que desea llevar: "));
+                        Carrito.push({nombre: Libros[3].nombre, precio: Libros[3].precio, Cantidad: cantidadQueVaAComprar});
+                        console.log("\nAgregado al carrito\n");
+                        alert("Agregado al carrito");
                     break;
                     case 5:
-                        suma = suma + precioJulioVerne;
-                        JulioVerne = true;
-                        cantidadJulioVerne++;
-                        console.log("\nAgregado 1 pack de Julio Verne\n");
-                        alert("Agregado 1 pack de Julio Verne");
+                        cantidadQueVaAComprar = parseInt(prompt("Ingrese la cantidad que desea llevar: "));
+                        Carrito.push({nombre: Libros[4].nombre, precio: Libros[4].precio, Cantidad: cantidadQueVaAComprar});
+                        console.log("\nAgregado al carrito\n");
+                        alert("Agregado al carrito");
                     break;
                 }
             }
@@ -140,68 +127,72 @@ do{
             console.log("\nOpción elegida: (2 - Mostrar carrito de compra)\n");
             alert("Opción elegida: (2 - Mostrar carrito de compra)");
 
-            console.log("\nContenido total del carrito: \n\nSaga completa de Harry Potter\n  Cantidad: " + cantidadHarryPotter + "   Precio unitario: $" + precioHarryPotter + "   total: $" + precioHarryPotter*cantidadHarryPotter + "\n" + "Saga completa de Maze Runner\n  Cantidad: " + cantidadMazeRunner + "   Precio unitario: $" + precioMazeRunner + "   total: $" + precioMazeRunner*cantidadMazeRunner + "\n" + "Saga completa de Sherlock Holmes\n  Cantidad: " + cantidadSherlockHolmes + "   Precio unitario: $" + precioSherlockHolmes + "   total: $" + precioSherlockHolmes*cantidadSherlockHolmes + "\n" + "Saga completa de Stephen King\n  Cantidad: " + cantidadStephenKing + "   Precio unitario: $" + precioStephenKing + "   total: $" + precioStephenKing*cantidadStephenKing + "\n" + "Saga completa de Julio Verne\n  Cantidad: " + cantidadJulioVerne + "   Precio unitario: $" + precioJulioVerne + "   total: $" + precioJulioVerne*cantidadJulioVerne + "\n\n" + "Total de la compra: $" + suma + "\n\n");
-            alert("Contenido total del carrito: \n\nSaga completa de Harry Potter\n  Cantidad: " + cantidadHarryPotter + "   Precio unitario: $" + precioHarryPotter + "   total: $" + precioHarryPotter*cantidadHarryPotter + "\n" + "Saga completa de Maze Runner\n  Cantidad: " + cantidadMazeRunner + "   Precio unitario: $" + precioMazeRunner + "   total: $" + precioMazeRunner*cantidadMazeRunner + "\n" + "Saga completa de Sherlock Holmes\n  Cantidad: " + cantidadSherlockHolmes + "   Precio unitario: $" + precioSherlockHolmes + "   total: $" + precioSherlockHolmes*cantidadSherlockHolmes + "\n" + "Saga completa de Stephen King\n  Cantidad: " + cantidadStephenKing + "   Precio unitario: $" + precioStephenKing + "   total: $" + precioStephenKing*cantidadStephenKing + "\n" + "Saga completa de Julio Verne\n  Cantidad: " + cantidadJulioVerne + "   Precio unitario: $" + precioJulioVerne + "   total: $" + precioJulioVerne*cantidadJulioVerne + "\n\n" + "Total de la compra: $" + suma);
+            Carrito.forEach((producto)=>{
+                console.log(producto);
+                // alert("Saga: " + producto.nombre + " Precio: $" + producto.precio + " Cantidad: " + producto.Cantidad);
+                arrayEnTextoParaUnSoloAlert = `${arrayEnTextoParaUnSoloAlert} \nSaga: ${producto.nombre} Precio: ${producto.precio} Cantidad: ${producto.Cantidad}`;
+            });
+            alert(arrayEnTextoParaUnSoloAlert);
             
         break;
         case 3:
-            console.log("\nOpción elegida: (3 - Cancelar la compra)\n");
-            alert("Opción elegida: (3 - Cancelar la compra)");
+            // console.log("\nOpción elegida: (3 - Cancelar la compra)\n");
+            // alert("Opción elegida: (3 - Cancelar la compra)");
 
-            console.log("\nDesea cancelar la compra? Escriba 'SI' para salir o 'NO' para para volver al menú\n");
-            preguntaCancelarCompra = prompt("\nDesea cancelar la compra? Escriba 'SI' para salir o 'NO' para para volver al menú\n");
-            preguntaCancelarCompra = preguntaCancelarCompra.toUpperCase();
+            // console.log("\nDesea cancelar la compra? Escriba 'SI' para salir o 'NO' para para volver al menú\n");
+            // preguntaCancelarCompra = prompt("\nDesea cancelar la compra? Escriba 'SI' para salir o 'NO' para para volver al menú\n");
+            // preguntaCancelarCompra = preguntaCancelarCompra.toUpperCase();
             
-            console.log("\nLa opción ingresada fue: " + preguntaCancelarCompra);
+            // console.log("\nLa opción ingresada fue: " + preguntaCancelarCompra);
 
-            if(preguntaCancelarCompra === "SI"){
-                console.log("\nCancelando...\n");
-                alert("Cancelando...");
-                cantidadHarryPotter = 0;
-                cantidadMazeRunner = 0;
-                cantidadSherlockHolmes = 0;
-                cantidadStephenKing = 0;
-                cantidadJulioVerne = 0;
+            // if(preguntaCancelarCompra === "SI"){
+            //     console.log("\nCancelando...\n");
+            //     alert("Cancelando...");
+            //     cantidadHarryPotter = 0;
+            //     cantidadMazeRunner = 0;
+            //     cantidadSherlockHolmes = 0;
+            //     cantidadStephenKing = 0;
+            //     cantidadJulioVerne = 0;
                 
-                HarryPotter = false;
-                MazeRunner = false;
-                SherlockHolmes = false;
-                StephenKing = false;
-                JulioVerne = false;
+            //     HarryPotter = false;
+            //     MazeRunner = false;
+            //     SherlockHolmes = false;
+            //     StephenKing = false;
+            //     JulioVerne = false;
 
-                console.log("\nCompra Cancelada con exito\n");
-                alert("Compra Cancelada con exito");
-            }else if(preguntaCancelarCompra === "NO"){
-                console.log("\nVolviendo al menú principal\n");
-                alert("\nVolviendo al menú principal\n");
-            }else{
-                console.log("\nLa opción ingresada es incorrecta\n");
-                alert("\nLa opción ingresada es incorrecta\n");
-            }
+            //     console.log("\nCompra Cancelada con exito\n");
+            //     alert("Compra Cancelada con exito");
+            // }else if(preguntaCancelarCompra === "NO"){
+            //     console.log("\nVolviendo al menú principal\n");
+            //     alert("\nVolviendo al menú principal\n");
+            // }else{
+            //     console.log("\nLa opción ingresada es incorrecta\n");
+            //     alert("\nLa opción ingresada es incorrecta\n");
+            // }
         break;
         case 4:
-            console.log("\nOpción elegida: (4 - Realizar la compra)\n");
-            alert("Opción elegida: (4 - Realizar la compra)");
+            // console.log("\nOpción elegida: (4 - Realizar la compra)\n");
+            // alert("Opción elegida: (4 - Realizar la compra)");
 
-            console.log("\nDesea realizar la compra? Escriba 'SI' para seguir o 'NO' para para volver al menú\n");
-            preguntaRealizarCompra = prompt("\nDesea realizar la compra? Escriba 'SI' para seguir o 'NO' para para volver al menú\n");
-            preguntaRealizarCompra = preguntaRealizarCompra.toUpperCase();
+            // console.log("\nDesea realizar la compra? Escriba 'SI' para seguir o 'NO' para para volver al menú\n");
+            // preguntaRealizarCompra = prompt("\nDesea realizar la compra? Escriba 'SI' para seguir o 'NO' para para volver al menú\n");
+            // preguntaRealizarCompra = preguntaRealizarCompra.toUpperCase();
             
-            console.log("\nLa opción ingresada fue: " + preguntaRealizarCompra);
+            // console.log("\nLa opción ingresada fue: " + preguntaRealizarCompra);
 
-            if(preguntaRealizarCompra === "SI"){
-                console.log("\nProcesando...\n");
-                alert("Procesando...");
+            // if(preguntaRealizarCompra === "SI"){
+            //     console.log("\nProcesando...\n");
+            //     alert("Procesando...");
                 
-                console.log("\nCompra Realizada con exito\n");
-                alert("Compra Realizada con exito");
-            }else if(preguntaRealizarCompra === "NO"){
-                console.log("\nVolviendo al menú principal\n");
-                alert("\nVolviendo al menú principal\n");
-            }else{
-                console.log("\nLa opción ingresada es incorrecta\n");
-                alert("\nLa opción ingresada es incorrecta\n");
-            }
+            //     console.log("\nCompra Realizada con exito\n");
+            //     alert("Compra Realizada con exito");
+            // }else if(preguntaRealizarCompra === "NO"){
+            //     console.log("\nVolviendo al menú principal\n");
+            //     alert("\nVolviendo al menú principal\n");
+            // }else{
+            //     console.log("\nLa opción ingresada es incorrecta\n");
+            //     alert("\nLa opción ingresada es incorrecta\n");
+            // }
             
         break;
         case 5:
@@ -232,4 +223,3 @@ do{
 
 }while(salir != true);
 
-*/
